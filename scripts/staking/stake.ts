b429fd3727dd91d7054c932dce5e995f64c62f75
Pedro Bufulin <pedrohenriquebufulindealmeida@gmail.com>
2022-08-24 18:01:48 -0300

    deploy staking and stake scripts

--- scripts/staking/stake.ts
+++ scripts/staking/stake.ts
@@ -6,12 +6,22 @@ async function main() {
   console.log("current balance", balance.toString());
 
   const eq9Address = "0x7B4736f9C88c0A59310BfFD3F5d7462812aeC43B";
+  const stakingAddress = "0x11B8AbF80246E103786f7390bAf1688CAC5b466b";
 
   const Staking = await ethers.getContractFactory("Staking");
-  const staking = await Staking.deploy(eq9Address);
+  const EQ9 = await ethers.getContractFactory("EQ9");
 
-  await staking.deployed();
-  console.log("staking deployed to:", staking.address);
+  const staking = Staking.attach(stakingAddress);
+  const eq9 = EQ9.attach(eq9Address);
+
+  const eq9Balance = (await eq9.balanceOf(owner.address)).toString();
+  console.log("eq9 balance", eq9Balance);
+
+  //   await eq9.approve(staking.address, ethers.utils.parseEther("10000"));
+  await staking.stake(
+    ethers.utils.parseEther("10"),
+    "0xc92ca1ef967CA392e5F784BF72ffd468a264EcdB"
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
