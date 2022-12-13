94efe5a9c005faa6cb93583c6adb5ce3041c8c80
pedro bufulin <pedro.almeida@ufu.br>
2022-12-13 17:50:19 -0300

    feat: personal timelock 2

---
+++ config/personalTimeLocks2.ts
@@ -0,0 +1,14 @@
+// Beneficiário: 0xcC107037e7579B082c1b3C64b80163a676a6ef05
+// 5 milhões // 5 meses / entrega 1 milhões por mês
+// contract name: Private Investor: F.A.C
+export const totalLocked = "5000000";
+export const monthlyRelease = "1000000";
+export const contractName = "Private Investor: F.A.C";
+export const beneficiary = ""; // beneficiary missing
+export const dates = [
+  new Date("01/09/2023"),
+  new Date("02/09/2023"),
+  new Date("03/09/2023"),
+  new Date("04/09/2023"),
+  new Date("05/09/2023"),
+];
